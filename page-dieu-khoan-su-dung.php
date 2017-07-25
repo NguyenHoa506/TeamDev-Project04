c30d7f8dbb330aa5bafd6094567e059fcaef244c
huongnguyen071723 <huongnguyen071723@gmail.com>
2017-07-25 11:42:55 +0700

    update css page-dieu-khoan-su-dung

--- page-dieu-khoan-su-dung.php
+++ page-dieu-khoan-su-dung.php
@@ -1,97 +1,71 @@
     <?php get_header(); ?>
     <div class="body-content">
-        <div id="action-banner" style="position:fixed; bottom:0; left:0;text-align:center;background-color:#6ab139; color:#fff; width:100%;padding:10px;z-index:10000;font-size:15px">
+        <div id="action-banner" class="bannerdiv">
             <i class="fa fa-phone" aria-hidden="true"></i> Miễn phí tư vấn <b>0901 395 886</b>
-        </div>
-<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/main.js" tppabs="http://magica.top/Scripts/main.js"></script>
-
-
-<!-- Hoa start -->
-    <div>
-        
+        </div>        
         <div id="mini-action" class="callToAction">
-            <div onclick="ShowAction()" class = "eModal-1" style="text-align:center; font-size:20px; cursor:pointer"><i class="fa fa-comments" aria-hidden="true"></i> Miễn phí tư vấn</div>
+            <div onclick="ShowAction()" class = "eModal-1 minidiv"><i class="fa fa-comments" aria-hidden="true"></i> Miễn phí tư vấn</div>
         </div>
-
 		<?php echo do_shortcode('[contact-form-7 id="309" title="Form-tu-van"]'); ?>
-
-
-<div class="col-md-12" style="padding: 0px">
-    <div id="banner" style="position:relative">
-        <div style="background-color:black; opacity:0.3; position:absolute; top:0;left:0;width:100%;height:100%;z-index:100;"></div>
-        <div class="head-bg-dieu-khoan-sd"></div>
-        <div id="ani-1" style="position: absolute; left: 0;color: #fff; z-index: 200;top:0">
-            <div style="position: relative; left: -25%;">
-                <h1 id="toptext">
-                    <span>ĐIỀU KHOẢN SỬ DỤNG</span>
-                </h1>
-
+        <div class="col-md-12" id="banner-dieu-khoan">
+            <div id="banner">
+                <div class="head-bg-dieu-khoan-sd"></div>
+                <div id="ani-1" class="noi-dung-banner-dieu-khoan">
+                    <div id="toptext-dieu-khoan">
+                        <h1 id="toptext">
+                            <span>ĐIỀU KHOẢN SỬ DỤNG</span>
+                        </h1>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 
-</div>
+        <div class="col-md-12" id="noi-dung-dieu-khoan">
+            <div class="row">
+                <div class="col-md-10 col-lg-offset-1">
+                    Chúng tôi có thể cập nhật các điều khoản thỏa thuận về sử dụng Trang Web mà không cần thông báo cho bạn. Điều đó đồng nghĩa việc bạn sử dụng trang web này là bạn đồng ý với các điều khoản sau đây.
+                    <br /><strong>1. Trách nhiệm của người sử dụng:</strong>
+                    <br />Khi truy cập vào trang web bạn phải cam kết rằng mình có đủ tuổi luật định và không phải là người bị hạn chế nhận các dịch vụ theo pháp luật để xác lập một hợp đồng có tính ràng buộc này.
+                    <br />Bạn hiểu và đồng ý chấp nhận các chi phí cho việc tiếp cận trang web, việc tiếp cận này liên quan đến việc trả phí của bên thứ ba (như phí cho nhà cung cấp dịch vụ internet hoặc phí đường truyền, phí pháp lý, phí tư vấn hoặc các khoản chi phí khác) có thể phát sinh trực tiếp hoặc gián tiếp.
+                    <br /> Bạn cũng hiểu và tự chịu trách nhiệm hoàn toàn đối với các rủi ro có thể xãy ra cho việc bạn truy cập trang web, những rủi ro này liên quan đến những thiệt hại cho máy tính của bạn, hoặc thiết bị khác hoặc việc mất các dữ liệu do việc tải xuống các tài liệu bị virus; hành động phá hoại trực tiếp hay gián tiếp của hệ thống máy tính khác, đường dây điện thoại, phần mềm, lỗi chương trình hoặc bất kì các lỗi nào khác…
+                    <br /> Bạn hiểu và đồng ý rằng Dịch Vụ có thể bao gồm các thông tin quảng cáo, thông tin liên lạc của URL mà các thông tin này là một phần trong việc tham gia trang web của chúng tôi và bạn sẽ không thể từ chối nhận các thông tin này.
+                    <br /><strong> 2. Nội dung trang web:</strong>
+                    <br />Chúng tôi lựa chọn kỹ lưỡng thông tin từ các nguồn tin cậy để cung cấp cho bạn đúng như bản thân sự việc nhưng chúng tôi không đảm bảo tính mới nhất, chính xác và đầy đủ hoàn toàn mặc dù chúng tôi cố gắng cập nhật thường xuyên các nội dung tại trang web.
+                    <br /><strong>3. Quyền sở hữu:</strong>
+                    <br />Chúng tôi là chủ sở hữu của trang web này. Bất cứ phần mềm được sử dụng và thông tin giới thiệu cho bạn qua trang web đều được bảo vệ bởi luật bản quyền và sở hữu trí tuệ hiện hành.
+                    <br />Chúng tôi có thẩm quyền chỉnh sửa trang web, cập nhật, thay đổi, xóa bỏ hoặc chấm dứt ngay bất kỳ nội dung nào mà không cần thông báo cho người sử dụng.
+                    <br />Việc sửa đổi, điều chỉnh, sao chép, thiết kế lại, đối chiếu, lắp ráp hoặc tìm cách phát hiện các mã nguồn, phá hoại, cho phép người khác có quyền lợi bảo mật hoặc bất kỳ mục đích, hình thức nào mà chúng tôi cho rằng không thích hợp đều là vi phạm quyền sở hữu hợp pháp của chúng tôi. Tùy theo từng trường hợp cụ thể chúng tôi sẽ lập tức chấm dứt mà không cần thông báo trước việc sử dụng tài khoản URL, bất kỳ địa chỉ email liên kết và truy cập trang web.
+                    <br /><strong>4. Sử dụng nội dung đưa lên hoặc phát trên trang web:</strong>
 
-<div class="col-md-12" style="background-color:#fff; padding:30px 15px;">
-
-    <div class="row">
-        <div class="col-md-10 col-lg-offset-1">
-            Chúng tôi có thể cập nhật các điều khoản thỏa thuận về sử dụng Trang Web mà không cần thông báo cho bạn. Điều đó đồng nghĩa việc bạn sử dụng trang web này là bạn đồng ý với các điều khoản sau đây.
-            <br /><strong>1. Trách nhiệm của người sử dụng:</strong>
-            <br />Khi truy cập vào trang web bạn phải cam kết rằng mình có đủ tuổi luật định và không phải là người bị hạn chế nhận các dịch vụ theo pháp luật để xác lập một hợp đồng có tính ràng buộc này.
-            <br />Bạn hiểu và đồng ý chấp nhận các chi phí cho việc tiếp cận trang web, việc tiếp cận này liên quan đến việc trả phí của bên thứ ba (như phí cho nhà cung cấp dịch vụ internet hoặc phí đường truyền, phí pháp lý, phí tư vấn hoặc các khoản chi phí khác) có thể phát sinh trực tiếp hoặc gián tiếp.
-            <br /> Bạn cũng hiểu và tự chịu trách nhiệm hoàn toàn đối với các rủi ro có thể xãy ra cho việc bạn truy cập trang web, những rủi ro này liên quan đến những thiệt hại cho máy tính của bạn, hoặc thiết bị khác hoặc việc mất các dữ liệu do việc tải xuống các tài liệu bị virus; hành động phá hoại trực tiếp hay gián tiếp của hệ thống máy tính khác, đường dây điện thoại, phần mềm, lỗi chương trình hoặc bất kì các lỗi nào khác…
-            <br /> Bạn hiểu và đồng ý rằng Dịch Vụ có thể bao gồm các thông tin quảng cáo, thông tin liên lạc của URL mà các thông tin này là một phần trong việc tham gia trang web của chúng tôi và bạn sẽ không thể từ chối nhận các thông tin này.
-            <br /><strong> 2. Nội dung trang web:</strong>
-            <br />Chúng tôi lựa chọn kỹ lưỡng thông tin từ các nguồn tin cậy để cung cấp cho bạn đúng như bản thân sự việc nhưng chúng tôi không đảm bảo tính mới nhất, chính xác và đầy đủ hoàn toàn mặc dù chúng tôi cố gắng cập nhật thường xuyên các nội dung tại trang web.
-            <br /><strong>3. Quyền sở hữu:</strong>
-            <br />Chúng tôi là chủ sở hữu của trang web này. Bất cứ phần mềm được sử dụng và thông tin giới thiệu cho bạn qua trang web đều được bảo vệ bởi luật bản quyền và sở hữu trí tuệ hiện hành.
-            <br />Chúng tôi có thẩm quyền chỉnh sửa trang web, cập nhật, thay đổi, xóa bỏ hoặc chấm dứt ngay bất kỳ nội dung nào mà không cần thông báo cho người sử dụng.
-            <br />Việc sửa đổi, điều chỉnh, sao chép, thiết kế lại, đối chiếu, lắp ráp hoặc tìm cách phát hiện các mã nguồn, phá hoại, cho phép người khác có quyền lợi bảo mật hoặc bất kỳ mục đích, hình thức nào mà chúng tôi cho rằng không thích hợp đều là vi phạm quyền sở hữu hợp pháp của chúng tôi. Tùy theo từng trường hợp cụ thể chúng tôi sẽ lập tức chấm dứt mà không cần thông báo trước việc sử dụng tài khoản URL, bất kỳ địa chỉ email liên kết và truy cập trang web.
-            <br /><strong>4. Sử dụng nội dung đưa lên hoặc phát trên trang web:</strong>
-
-            <br />Việc bạn gửi cho chúng tôi bất kỳ thông tin có chứa đựng những ý tưởng, minh họa, sáng chế, triển khai, đề xuất đồng nghĩa với việc bạn đồng ý với những điều sau đây:
-            <br />i. Chúng tôi không buộc phải xem những thông tin này là thông tin mật và không có trách nhiệm lưu trữ chúng.
-            <br />ii. Chúng tôi không có trách nhiệm nghiên cứu, đánh giá hay áp dụng thông tin này.
-            <br /> iii. Cho dù chúng tôi vô tình áp dụng một ý tưởng gần giống hay tương tự một phần hay toàn bộ, với ý tưởng của bạn, chúng tôi không có trách nhiệm phải bồi thường bất kỳ thiệt hại nào cho những sự việc như vậy.
-            <br />Chúng tôi cũng có thể dùng thông tin cá nhân thu thập được từ bạn để thông báo cho bạn về những sản phẩm và dịch vụ, chương trình khuyến mãi hay các công ty đối tác cung cấp, hoặc để xin ý kiến của bạn về các sản phẩm và dịch vụ hiện tại hay những sản phẩm và dịch vụ tiềm năng mới.
-            <br />Chúng tôi có một khu vực để bạn có thể liên lạc với chúng tôi. Bất kỳ phản hồi nào bạn gửi đến trang web sẽ trở thành tài sản của chúng tôi và chúng tôi có thể dùng phản hồi đó (chẳng hạn các câu chuyện thành công) cho các mục đích tiếp thị, hoặc liên hệ với bạn để có thêm thông tin.
-            <br />Chúng tôi cố gắng hết sức bảo vệ thông tin cá nhân của bạn bao gồm việc không tiết lộ hoặc gửi cho bất kỳ bên thứ ba nào. Không có dữ liệu nào truyền trên internet được bảo mật an toàn 100% nên bạn tự chịu rủi ro này.
-            <strong>5. Việc tải dữ liệu:</strong>
-            <br />Bạn chỉ có quyền sử dụng (cho công tác nghiên cứu, học tập hoặc giảng dạy) chứ không có quyền sở hữu những phần mềm và dữ liệu đã tải từ trang web của chúng tôi cũng như không được phép bán, phân phối lại hay bẻ khóa phần mềm.Trong trường hợp này bạn phải viện dẫn nguồn từ đâu.
-            <br />Trong trường hợp bạn muốn sử dụng vì mục đích thương mại thì phải được sự cho phép bằng văn bản của chúng tôi.
-            <br /><strong>6. Hạn chế đưa thông tin lên trang web:</strong>
-            <br />Chúng tôi liên tục kiểm soát và loại bỏ ngay lập tức các thông tin có tính:
-            <br />i. Chống phá nhà nước CHXHCN Việt Nam; phạm pháp hình sự.
-            <br />ii. Xâm phạm quyền tự do cá nhân của người khác; và/hoặc làm nhục, phỉ báng, bôi nhọ người khác; và/hoặc gây phương hại hay gây bất lợi cho người khác.
-            <br />iii. Truyền bá và phân phối thông tin cá nhân của bên thứ ba mà không được sự chấp thuận của họ.
-            <br />iv. Truyền đi những tập tin máy tính bị nhiễm virus gây hư hại hoạt động của các máy tính khác.
-            <br />v. Sử dụng các loại robot, nhện máy (spiders) và/hoặc bất kỳ thiết bị tự động nào, và/hoặc tự tay theo dõi và thu thập tài liệu của website cho bất kỳ mục đích tái sử dụng mà không được sự cho phép trước bằng văn bản của chúng tôi.
-            <br />vi. Sử dụng bất kỳ thiết bị, phần mềm và/hoặc tiến trình nào nhằm xâm phạm hoặc cố ý xâm phạm đến hoạt động của website.
-            <br />vii. Bất kỳ hành động nào không hợp pháp và/hoặc bị cấm bởi các bộ luật tương ứng.
-            <br />viii. Bất kỳ hành động nào mà chúng tôi cho rằng không thích hợp.
-            <br /><strong>7. Liên kết với trang web khác</strong>
-            <br />Trang web này có thể được liên kết với các trang web khác thuộc Magica.top. Do đó bên cạnh những quy định trong các điều khoản này bạn còn phải chịu sự điều chỉnh của các điều khoản riêng từ các trang web đó.
-            <br />Chúng tôi sẽ không chịu bất cứ trách nhiệm pháp lý hay bất cứ sự tham chiếu nào tới một trang web không thuộc Magica.top và/hoặc tới một sản phẩm và/hoặc thông tin đặc biệt nào của bên thứ ba trên trang web này.
-            <br /><strong>8. Luật áp dụng</strong>
-            <br />Trang web này được sự điều chỉnh của pháp luật nước CHXHCN Việt Nam.
+                    <br />Việc bạn gửi cho chúng tôi bất kỳ thông tin có chứa đựng những ý tưởng, minh họa, sáng chế, triển khai, đề xuất đồng nghĩa với việc bạn đồng ý với những điều sau đây:
+                    <br />i. Chúng tôi không buộc phải xem những thông tin này là thông tin mật và không có trách nhiệm lưu trữ chúng.
+                    <br />ii. Chúng tôi không có trách nhiệm nghiên cứu, đánh giá hay áp dụng thông tin này.
+                    <br /> iii. Cho dù chúng tôi vô tình áp dụng một ý tưởng gần giống hay tương tự một phần hay toàn bộ, với ý tưởng của bạn, chúng tôi không có trách nhiệm phải bồi thường bất kỳ thiệt hại nào cho những sự việc như vậy.
+                    <br />Chúng tôi cũng có thể dùng thông tin cá nhân thu thập được từ bạn để thông báo cho bạn về những sản phẩm và dịch vụ, chương trình khuyến mãi hay các công ty đối tác cung cấp, hoặc để xin ý kiến của bạn về các sản phẩm và dịch vụ hiện tại hay những sản phẩm và dịch vụ tiềm năng mới.
+                    <br />Chúng tôi có một khu vực để bạn có thể liên lạc với chúng tôi. Bất kỳ phản hồi nào bạn gửi đến trang web sẽ trở thành tài sản của chúng tôi và chúng tôi có thể dùng phản hồi đó (chẳng hạn các câu chuyện thành công) cho các mục đích tiếp thị, hoặc liên hệ với bạn để có thêm thông tin.
+                    <br />Chúng tôi cố gắng hết sức bảo vệ thông tin cá nhân của bạn bao gồm việc không tiết lộ hoặc gửi cho bất kỳ bên thứ ba nào. Không có dữ liệu nào truyền trên internet được bảo mật an toàn 100% nên bạn tự chịu rủi ro này.
+                    <strong>5. Việc tải dữ liệu:</strong>
+                    <br />Bạn chỉ có quyền sử dụng (cho công tác nghiên cứu, học tập hoặc giảng dạy) chứ không có quyền sở hữu những phần mềm và dữ liệu đã tải từ trang web của chúng tôi cũng như không được phép bán, phân phối lại hay bẻ khóa phần mềm.Trong trường hợp này bạn phải viện dẫn nguồn từ đâu.
+                    <br />Trong trường hợp bạn muốn sử dụng vì mục đích thương mại thì phải được sự cho phép bằng văn bản của chúng tôi.
+                    <br /><strong>6. Hạn chế đưa thông tin lên trang web:</strong>
+                    <br />Chúng tôi liên tục kiểm soát và loại bỏ ngay lập tức các thông tin có tính:
+                    <br />i. Chống phá nhà nước CHXHCN Việt Nam; phạm pháp hình sự.
+                    <br />ii. Xâm phạm quyền tự do cá nhân của người khác; và/hoặc làm nhục, phỉ báng, bôi nhọ người khác; và/hoặc gây phương hại hay gây bất lợi cho người khác.
+                    <br />iii. Truyền bá và phân phối thông tin cá nhân của bên thứ ba mà không được sự chấp thuận của họ.
+                    <br />iv. Truyền đi những tập tin máy tính bị nhiễm virus gây hư hại hoạt động của các máy tính khác.
+                    <br />v. Sử dụng các loại robot, nhện máy (spiders) và/hoặc bất kỳ thiết bị tự động nào, và/hoặc tự tay theo dõi và thu thập tài liệu của website cho bất kỳ mục đích tái sử dụng mà không được sự cho phép trước bằng văn bản của chúng tôi.
+                    <br />vi. Sử dụng bất kỳ thiết bị, phần mềm và/hoặc tiến trình nào nhằm xâm phạm hoặc cố ý xâm phạm đến hoạt động của website.
+                    <br />vii. Bất kỳ hành động nào không hợp pháp và/hoặc bị cấm bởi các bộ luật tương ứng.
+                    <br />viii. Bất kỳ hành động nào mà chúng tôi cho rằng không thích hợp.
+                    <br /><strong>7. Liên kết với trang web khác</strong>
+                    <br />Trang web này có thể được liên kết với các trang web khác thuộc Magica.top. Do đó bên cạnh những quy định trong các điều khoản này bạn còn phải chịu sự điều chỉnh của các điều khoản riêng từ các trang web đó.
+                    <br />Chúng tôi sẽ không chịu bất cứ trách nhiệm pháp lý hay bất cứ sự tham chiếu nào tới một trang web không thuộc Magica.top và/hoặc tới một sản phẩm và/hoặc thông tin đặc biệt nào của bên thứ ba trên trang web này.
+                    <br /><strong>8. Luật áp dụng</strong>
+                    <br />Trang web này được sự điều chỉnh của pháp luật nước CHXHCN Việt Nam.
+                </div>
+            </div>
         </div>
-
-    </div>
-
-</div>
-
-        
         <?php get_footer(); ?>
     </div>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-79228914-1', 'auto');
-      ga('send', 'pageview');
-    </script>
 </body>
 </html>
