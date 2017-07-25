2353eb7b45b069132efeaa8d2153cf3bdf33af6b
nk\Hoa Oanh <1411270@hcmut.edu.vn>
2017-07-25 11:17:09 +0700

    google ads

--- page-google-ads.php
+++ page-google-ads.php
@@ -22,7 +22,7 @@
         <?php echo do_shortcode('[contact-form-7 id="309" title="Form-tu-van"]'); ?>
     
     <div class="col-md-12 paddingzero">
-        <div id="banner" class="bannerd">
+        <div id="banner" class="position_rdiv">
             <div class="vonghia"></div>
             <div class="head-bg-ads"></div>
             <div id="ani-1" class="ani-1-div">
@@ -43,7 +43,7 @@
     
     <div id="price" class="col-md-12 block bgrdeee">
         <div class="col-md-12 centdiv">
-            <h2 style="margin-bottom:20px">Các Gói Dịch Vụ</h2>
+            <h2 class="marginbtdiv2">Các Gói Dịch Vụ</h2>
             <div>
                 <div class="row">
                     <div class="col-md-12">
@@ -125,33 +125,33 @@
             </div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 centdiv">
                 <div class="meas">
-                    <p style="margin-top:15px"><i class="fa fa-fw fa-4x fa-usd" aria-hidden="true"></i></p>
+                    <p class="margintdiv"><i class="fa fa-fw fa-4x fa-usd" aria-hidden="true"></i></p>
                 </div>
-                <h4 style="font-size:18px">
+                <h4 class="fsizediv">
                     Doanh thu
                 </h4>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 centdiv">
                 <div class="meas">
-                    <p style="margin-top:15px"><i class="fa fa-fw fa-4x fa-users" aria-hidden="true"></i></p>
+                    <p class="margintdiv"><i class="fa fa-fw fa-4x fa-users" aria-hidden="true"></i></p>
                 </div>
-                <h4 style="font-size:18px">
+                <h4 class="fsizediv">
                     Tỷ lệ chuyển đổi khách hàng
                 </h4>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 centdiv">
                 <div class="meas">
-                    <p style="margin-top:15px"><i class="fa fa-fw fa-4x fa-user-plus" aria-hidden="true"></i></p>
+                    <p class="margintdiv"><i class="fa fa-fw fa-4x fa-user-plus" aria-hidden="true"></i></p>
                 </div>
-                <h4 style="font-size:18px">
+                <h4 class="fsizediv">
                     Số khách hàng tiềm năng truy cập
                 </h4>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 centdiv">
                 <div class="meas">
-                    <p style="margin-top:15px"><i class="fa fa-fw fa-4x fa-hand-pointer-o" aria-hidden="true"></i></p>
+                    <p class="margintdiv"><i class="fa fa-fw fa-4x fa-hand-pointer-o" aria-hidden="true"></i></p>
                 </div>
-                <h4 style="font-size:18px">
+                <h4 class="fsizediv">
                     Các chỉ số hành vi trên website
                 </h4>
             </div>
@@ -159,7 +159,7 @@
 
     </div>
 
-    <div id="feature" class="col-md-12" style="background-color:#eee">
+    <div id="feature" class="col-md-12 bgrdeee">
         <div class="margin-detail col-md-12">
             <h2 class="prof">Chúng Tôi Sẽ Giúp Bạn</h2>
         </div>
@@ -227,7 +227,7 @@
                             <p>Nhờ kinh nghiệm lâu năm, khả năng thấu hiểu khách hàng để quảng cáo đúng mục tiêu, khả năng xử lý dữ liệu lớn của Google</p>
                         </div>                       
                     </div>
-                    <div class="col-lg-6 col-md-6 hover-c" style="position:relative">
+                    <div class="col-lg-6 col-md-6 hover-c position_rdiv">
                         <div class="col-md-3" style="text-align:center; padding-top:25px">
                             <i class="fa fa-fw fa-4x fa-line-chart" aria-hidden="true"></i>
                         </div>
@@ -236,7 +236,7 @@
                             <p>Không đơn thuần là quảng cáo Google, chúng tôi còn tìm hiểu về đặc thù ngành và chiến lược công ty bạn để quảng cáo Google phải đồng bộ với chiến lược kinh doanh của bạn</p>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 hover-c" style="position:relative">
+                    <div class="col-lg-6 col-md-6 hover-c position_rdiv">
                         <div class="col-md-3" style="text-align:center; padding-top:25px">
                             <i class="fa fa-fw fa-4x fa-random" aria-hidden="true"></i>
                         </div>
@@ -245,7 +245,7 @@
                             <p>Không đơn thuần là quảng cáo Google, chúng tôi còn tìm hiểu về đặc thù ngành và chiến lược công ty bạn để quảng cáo Google phải đồng bộ với chiến lược kinh doanh của bạn</p>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 hover-c" style=" position:relative">
+                    <div class="col-lg-6 col-md-6 hover-c position_rdiv">
                         <div class="col-md-3" style="text-align:center; padding-top:25px">
                             <i class="fa fa-fw fa-4x fa-cogs" aria-hidden="true"></i>
                         </div>
@@ -295,7 +295,7 @@
 
     <div id="flow" class="col-md-12" style="background-color:rgb(0, 107, 156); color:#fff;">
         <div class="col-md-12 centdiv">
-            <h2 style="margin-bottom:20px">Quy trình làm việc</h2>
+            <h2 class="marginbtdiv2">Quy trình làm việc</h2>
             <div class="row">
                 <div class="col-md-12">
                     <div class="col-md-10 col-md-offset-1">
@@ -307,13 +307,13 @@
                                             <div>
                                                 <span class="fa-stack fa-5x">
                                                     <span class="step-num-ads"><span>1</span></span>
-                                                    <i style="color:#fff" class="fa fa-circle fa-stack-2x"></i>
-                                                    <i style="color:#fff" class="fa fa-circle-thin fa-stack-2x"></i>
+                                                    <i class="fa fa-circle fa-stack-2x colorfff"></i>
+                                                    <i class="fa fa-circle-thin fa-stack-2x colorfff"></i>
                                                     <i style="color:rgb(0, 107, 156)" class="fa fa-list-ul fa-stack-1x"></i>
                                                 </span>
 
                                             </div>
-                                            <div style="font-size:20px">Nhận yêu cầu</div>
+                                            <div class="fsizediv2">Nhận yêu cầu</div>
                                         </div>
                                         <div class="step-desc">
                                             <p>Nhận yêu cầu. Giới thiệu tổng quan về quảng cáo Google Adwords. Giới thiệu tổng quan về nhu cầu tìm kiếm online của ngành nghề khách hàng, hiệu quả ước tính từ Google Adwords</p>
@@ -326,12 +326,12 @@
                                             <div>
                                                 <span class="fa-stack fa-5x">
                                                     <span class="step-num"><span>2</span></span>
-                                                    <i style="color:#fff" class="fa fa-circle fa-stack-2x"></i>
-                                                    <i style="color:#fff" class="fa fa-circle-thin fa-stack-2x"></i>
+                                                    <i class="fa fa-circle fa-stack-2x colorfff"></i>
+                                                    <i class="fa fa-circle-thin fa-stack-2x colorfff"></i>
                                                     <i style="color:rgb(0, 107, 156)" class="fa fa-files-o fa-stack-1x"></i>
                                                 </span>
                                             </div>
-                                            <div style="font-size:20px">Thống nhất giải pháp</div>
+                                            <div class="fsizediv2">Thống nhất giải pháp</div>
                                         </div>
                                         <div class="step-desc">
                                             <p>Tư vấn khách hàng về giải pháp. Thống nhát từ khóa. Thống nhất ngân sách</p>
@@ -344,12 +344,12 @@
                                             <div>
                                                 <span class="fa-stack fa-5x">
                                                     <span class="step-num"><span>3</span></span>
-                                                    <i style="color:#fff" class="fa fa-circle fa-stack-2x"></i>
-                                                    <i style="color:#fff" class="fa fa-circle-thin fa-stack-2x"></i>
+                                                    <i class="fa fa-circle fa-stack-2x colorfff"></i>
+                                                    <i class="fa fa-circle-thin fa-stack-2x colorfff"></i>
                                                     <i style="color:rgb(0, 107, 156)" class="fa fa-rocket fa-stack-1x"></i>
                                                 </span>
                                             </div>
-                                            <div style="font-size:20px">Set mẫu quảng cáo ban đầu</div>
+                                            <div class="fsizediv2">Set mẫu quảng cáo ban đầu</div>
                                         </div>
                                         <div class="step-desc">
                                             <p>Set hiển thị của quảng cáo. Set tính năng bên trong của mẫu quảng cáo</p>
@@ -366,12 +366,12 @@
                                             <div>
                                                 <span class="fa-stack fa-5x">
                                                     <span class="step-num"><span>4</span></span>
-                                                    <i style="color:#fff" class="fa fa-circle fa-stack-2x"></i>
-                                                    <i style="color:#fff" class="fa fa-circle-thin fa-stack-2x"></i>
+                                                    <i class="fa fa-circle fa-stack-2x colorfff"></i>
+                                                    <i class="fa fa-circle-thin fa-stack-2x colorfff"></i>
                                                     <i style="color:rgb(0, 107, 156)" class="fa fa-database fa-stack-1x"></i>
                                                 </span>
                                             </div>
-                                            <div style="font-size:20px">Tối ưu hóa mẫu quảng cáo</div>
+                                            <div class="fsizediv2">Tối ưu hóa mẫu quảng cáo</div>
                                         </div>
                                         <div class="step-desc">
                                             <p>Dựa trên dữ liệu liên tục được trả về từ Google. Việc tối ưu hóa có thể giúp tăng 2 - 5 lần hiệu quả theo thời gian</p>
@@ -384,12 +384,12 @@
                                             <div>
                                                 <span class="fa-stack fa-5x">
                                                     <span class="step-num"><span>5</span></span>
-                                                    <i style="color:#fff" class="fa fa-circle fa-stack-2x"></i>
-                                                    <i style="color:#fff" class="fa fa-circle-thin fa-stack-2x"></i>
+                                                    <i class="fa fa-circle fa-stack-2x colorfff"></i>
+                                                    <i class="fa fa-circle-thin fa-stack-2x colorfff"></i>
                                                     <i style="color:rgb(0, 107, 156)" class="fa fa-bar-chart fa-stack-1x"></i>
                                                 </span>
                                             </div>
-                                            <div style="font-size:20px">Báo cáo định kỳ</div>
+                                            <div class="fsizediv2">Báo cáo định kỳ</div>
                                         </div>
                                         <div class="step-desc">
                                             <p>Báo cáo theo tháng. Báo cáo về chi phí, số lượng click, tỷ lệ click vào website, số lượng chuyển đổi (nếu có), ...</p>
@@ -413,7 +413,7 @@
     <div id="achievement" class="col-md-12">
         <div class="achievementdiv"></div>
         <div class="col-md-12 centdiv">
-            <div style="margin-bottom:20px">
+            <div class="marginbtdiv2">
                 <h2>Thành Tích Của Chúng Tôi</h2>
                 <h4><small>GIẢI PHÁP QUẢNG CÁO HOÀN HẢO DÀNH CHO BẠN</small></h4>
             </div>
@@ -438,14 +438,14 @@
         <div class="col-md-10 col-lg-offset-1">
             <h2 style="text-align:center; margin: 40px 0;">Mọi Người Nói Gì Về Chúng Tôi</h2>
             <div class="col-md-4">
-                <div style="text-align:center">
-                    <img style="height:150px; width:150px; border-radius:50%" alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p1.jpg" tppabs="http://magica.top/Imgs/p1.jpg" />
+                <div class="centdiv">
+                    <img class="circleimg"> alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p1.jpg" tppabs="http://magica.top/Imgs/p1.jpg" />
                 </div>
                 <div style="text-align:center; color:#6ab139;margin:10px">
                     <i class="fa fa-fw fa-2x fa-quote-right"></i>
                 </div>
                 <div>
-                    <p style="text-align:justify">Sau khi chạy quảng cáo, lượng khách hàng của tôi tăng lên rõ rệt. Tôi không còn phải lo lắng về chiến lược quảng cáo.</p>               
+                    <p class="justdiv">Sau khi chạy quảng cáo, lượng khách hàng của tôi tăng lên rõ rệt. Tôi không còn phải lo lắng về chiến lược quảng cáo.</p>               
                     <h4 style="text-align:center; margin-bottom:0">
                         Anh Lê Khang
                     </h4>
@@ -454,26 +454,26 @@
             </div>
             <div class="col-md-4">
                 <div class="centdiv">
-                    <img style="height:150px; width:150px; border-radius:50%" alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p2.jpg" tppabs="http://magica.top/Imgs/p2.jpg" />
+                    <img class="circleimg"> alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p2.jpg" tppabs="http://magica.top/Imgs/p2.jpg" />
                 </div>
                 <div style="text-align:center;color:#6ab139;margin:10px">
                     <i class="fa fa-fw fa-2x fa-quote-right"></i>
                 </div>
                 <div>
-                    <p style="text-align:justify">Nhờ có quảng cáo Google, công ty đã giúp tôi có lượng khách hàng ổn định trong thời gian dài, giúp giảm chi phí</p>
+                    <p class="justdiv">Nhờ có quảng cáo Google, công ty đã giúp tôi có lượng khách hàng ổn định trong thời gian dài, giúp giảm chi phí</p>
                     <h4 style="text-align:center;margin-bottom:0">Chị Oanh Trần</h4>
                     <p style="color:#ccc; font-size:13px;text-align:center">Cty CP FirstA Web</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="centdiv">
-                    <img style="height:150px; width:150px; border-radius:50%" alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p3.jpg" tppabs="http://magica.top/Imgs/p3.jpg" />
+                    <img class="circleimg"> alt="Hình ảnh" src="<?php bloginfo('template_directory');?>/image/p3.jpg" tppabs="http://magica.top/Imgs/p3.jpg" />
                 </div>
                 <div style="text-align:center;color:#6ab139; margin:10px">
                     <i class="fa fa-fw fa-2x fa-quote-right"></i>
                 </div>
                 <div>
-                    <p style="text-align:justify">Giá rẻ nhưng lại rất hiệu quả. Cám ơn công ty Magic</p>
+                    <p class="justdiv">Giá rẻ nhưng lại rất hiệu quả. Cám ơn công ty Magic</p>
                     <h4 style="text-align:center; margin-bottom:0">Anh Phúc</h4>
                     <p style="color:#ccc; font-size:13px;text-align:center">Cty CP Xe Hơi Toàn Năng</p>
                 </div>
