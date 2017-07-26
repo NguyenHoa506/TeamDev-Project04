40ec962cbf5c685dac9d7c3f354c4a1ea4ea8983
Lam <51301989@hcmut.edu.vn>
2017-07-26 14:43:04 +0700

    index

--- page-thiet-ke-website.php
+++ page-thiet-ke-website.php
@@ -29,9 +29,9 @@
             <div class="head-bg-thiet-ke"></div>
             <div id="ani-1" class="idani-1" style="width:75%">
                 <div class="position-title-tkwebsite">
-                    <div id="toptext2">
+                    <div id="toptext2" style="margin-top: 0;">
                         <div class="pos-relative">
-                            <span class="smalldeco" style="position:absolute; left:0px; width:28%; bottom: 87px; text-align:right;">Không  chỉ là</span>
+                            <span class="smalldeco small-text" >Không  chỉ là</span>
                             <h2 class="smallH2 h2-text">Thiết kế website</h2>                           
                         </div>
                         <div class="pos-relative">
